6e52ab03029a88c3b281b4eec082743fa3dce133
Ahsan Shareef Mohammed <officialasmohammed06@gmail.com>
2025-08-14 07:16:41 -0400

    modified app.yaml and added data.sql file for multiple initial insert into DB

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into Product_Table(name,price,DESCRIPTION123,productType) values ("Samsung-Galaxy S24",1200,"Samsung Company","Electronics");
+
+insert into Product_Table(name,price,DESCRIPTION123,productType) values ("Iphone 11",400,"Apple Company","Electronics");
+
+insert into Product_Table(name,price,DESCRIPTION123,productType) values ("Sofa Set ",500,"Bobs Furniture","Furniture");
+
+insert into Product_Table(name,price,DESCRIPTION123,productType) values ("Kodak DSLR",1400,"KODAK Company","Electronics");
+
+insert into Product_Table(name,price,DESCRIPTION123,productType) values ("Samsung-Smart-TV",700,"Samsung Company","Electronics");
